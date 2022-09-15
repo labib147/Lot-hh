769894c3a1afe9e20cdcea4e4764de528c8b1300
labib147 <nefaurrahmanlabib147@gmail.com>
2022-09-15 12:57:21 +0600

    updated mock

--- deploy/deploy-mocks.js
+++ deploy/deploy-mocks.js
@@ -1,5 +1,13 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId // We will use network.name
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...")
+        // deploy a mock vrfcoordinator...
+    }
 }
