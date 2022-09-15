cf86e755b6b7ccde010e1490a8205ad7f53a47e7
labib147 <nefaurrahmanlabib147@gmail.com>
2022-09-15 12:52:00 +0600

    initiated mock

---
+++ deploy/deploy-mocks.js
@@ -0,0 +1,5 @@
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId // We will use network.name
+}
